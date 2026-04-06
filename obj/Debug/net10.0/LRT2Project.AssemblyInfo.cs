3bdbf9bb7e39ab63f3a0b0846fe69f218fc66363
Elexys Rae T. Diaz <your_email@example.com>
2026-04-06 17:39:35 +0800

    Added header comment for documentation

--- obj/Debug/net10.0/LRT2Project.AssemblyInfo.cs
+++ obj/Debug/net10.0/LRT2Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LRT2Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07db937d21ff0edb700bcb383764586619872407")]
 [assembly: System.Reflection.AssemblyProductAttribute("LRT2Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LRT2Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
